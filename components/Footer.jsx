652692e8cffc0c8fea2c7af1612f5f9ae31ba2a4
kingkuta080 <alubaidullahi080@gmail.com>
2025-03-06 14:52:57 +0100

    build

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+// good !
 import Link from "next/link";
 import Image from "next/image";
 import { Facebook, Twitter, Instagram, Mail, Phone, MapPin, Linkedin, Youtube } from "lucide-react";
