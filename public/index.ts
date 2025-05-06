33ddbe8858590975f06a05ec0e486182dc295a52
kingkuta080 <alubaidullahi080@gmail.com>
2025-05-06 01:09:49 +0100

    images fixed

--- public/index.ts
+++ public/index.ts
@@ -3,4 +3,4 @@ export {default as kudanfav} from './kudan_favicon.png';
 export {default as Logo} from './logo.png';
 export {default as Bg1} from './bg1.png';
 export {default as Bg2} from './bg2.png';
-export {default as Bg4} from './bg4.png';
+export {default as Bg4} from './bg4.jpg';
